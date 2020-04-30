86d8e64f8d3112a9da3909d6f91b6231561db48a
imejakeulating <zjake90@gmail.com>
2020-04-30 14:04:36 -0500

    basic gui for server and server code from class

---
+++ P4Server/src/main/java/Server.java
@@ -0,0 +1,124 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+import javafx.application.Platform;
+import javafx.scene.control.ListView;
+/*
+ * Clicker: A: I really get it    B: No idea what you are talking about
+ * C: kind of following
+ */
+
+public class Server{
+
+	int count = 1;	
+	int port;
+	ArrayList<ClientThread> clients = new ArrayList<ClientThread>();
+	TheServer server;
+	private Consumer<Serializable> callback;
+	
+	Server(int port) {
+		this.port = port;
+	}
+	
+	void setConsumer(Consumer<Serializable> call) {
+		callback = call;
+		callback.accept("Waiting for clients...");
+		server = new TheServer();
+		server.start();
+	}
+	
+	
+	public class TheServer extends Thread{
+		
+		public void run() {
+		    try {
+		    	
+			ServerSocket mySocket = new ServerSocket(port);
+		    System.out.println("Server is waiting for a client!");
+		  
+			
+		    while(true) {
+		
+				ClientThread c = new ClientThread(mySocket.accept(), count);
+				callback.accept("client has connected to server: " + "client #" + count);
+				clients.add(c);
+				c.start();
+				
+				count++;
+				
+			    }
+			}//end of try
+				catch(Exception e) {
+					callback.accept("Server socket did not launch");
+				}
+			}//end of while
+		}
+	
+
+		class ClientThread extends Thread{
+			
+		
+			Socket connection;
+			int count;
+			ObjectInputStream in;
+			ObjectOutputStream out;
+			
+			ClientThread(Socket s, int count){
+				this.connection = s;
+				this.count = count;	
+			}
+			
+			/*
+			 * When all clients need to recieve a message
+			 */
+			public void updateClients(String message) {
+				for(int i = 0; i < clients.size(); i++) {
+					ClientThread t = clients.get(i);
+					try {
+					 t.out.writeObject(message);
+					}
+					catch(Exception e) {}
+				}
+			}
+			
+			public void run(){
+					
+				try {
+					in = new ObjectInputStream(connection.getInputStream());
+					out = new ObjectOutputStream(connection.getOutputStream());
+					connection.setTcpNoDelay(true);	
+				}
+				catch(Exception e) {
+					System.out.println("Streams not open");
+				}
+				
+				updateClients("new client on server: client #"+count);
+					
+				 while(true) {
+					    try {
+					    	String data = in.readObject().toString();
+					    	callback.accept("client: " + count + " sent: " + data);
+					    	updateClients("client #"+count+" said: "+data);
+					    	
+					    	}
+					    catch(Exception e) {
+					    	callback.accept("OOOOPPs...Something wrong with the socket from client: " + count + "....closing down!");
+					    	updateClients("Client #"+count+" has left the server!");
+					    	clients.remove(this);
+					    	break;
+					    }
+					}
+				}//end of run
+			
+			
+		}//end of client thread
+}
+
+
+	
+	
--- P4Server/src/main/java/WordGuessServer.java
+++ P4Server/src/main/java/WordGuessServer.java
@@ -1,9 +1,31 @@
+import java.util.HashMap;
+
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class WordGuessServer extends Application {
+
+	private Button startServer;
+	private TextField portText;
+	private Label label;
+	private ListView<String> listView;
+	private HashMap<String, Scene> sceneMap;
+	private ObservableList<String> listContents;
+	private Server serverConnection;
+	private PauseTransition pause = new PauseTransition(Duration.seconds(.75));
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -14,11 +36,65 @@ public class WordGuessServer extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		primaryStage.setTitle("(server) Playing word guess!!!");
+		primaryStage.setTitle("(Server) Let's Play Word Guess!");
 		
-		Scene scene = new Scene(new HBox(),600,600);
-		primaryStage.setScene(scene);
+		//Add the 2 scenes to the scene map
+		sceneMap = new HashMap<String, Scene>();
+		sceneMap.put("start", createStartScene());
+		sceneMap.put("info", createInfoScene());
+		
+		//Add functionality to the start server button
+		startServer.setOnAction(e -> pause.play());
+		pause.setOnFinished(e -> {
+			//Implement action only if the user enters a port #!!!
+			if(portText.getText() != null) {
+				
+				primaryStage.setScene(sceneMap.get("info"));
+				primaryStage.setTitle("Game Info Page");
+				
+				serverConnection = new Server(Integer.parseInt(portText.getText()));
+				serverConnection.setConsumer(data -> {
+					Platform.runLater(() -> {
+						listView.getItems().add(data.toString());
+					});
+				});
+				
+			}
+		});
+		
+			
+		primaryStage.setScene(sceneMap.get("start"));
 		primaryStage.show();
 	}
-
+	
+	
+	
+	//Initialize the starting scene and return it
+	public Scene createStartScene() {
+		
+		label = new Label("Enter a port number.");
+		portText = new TextField();
+	
+		startServer = new Button("Start Server");
+		
+		VBox box = new VBox(70, label, portText, startServer);
+		
+		box.setAlignment(Pos.CENTER);
+		
+		return new Scene(box, 500,500);
+		
+	}
+	
+	public Scene createInfoScene() {
+		
+		listView = new ListView<String>();
+		listContents = FXCollections.observableArrayList();
+		
+		VBox box = new VBox(listView);
+		
+		box.setAlignment(Pos.CENTER);
+		
+		return new Scene(box,500,500);
+	}
+	
 }
